3a552f5cf15bb4a229970aff48f44689e9cde419
Hansel Omondi <ogopabhs@gmail.com>
2024-12-04 15:08:59 +0300

    feat: add() method in Fuata class
    This method will handle the fuata add <filename> command used to stage
    files

--- src/main/kotlin/Fuata.kt
+++ src/main/kotlin/Fuata.kt
@@ -1,9 +1,17 @@
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import model.IndexEntry
+import util.CommitUtil
+import util.Compression
+import util.Indexing
 import java.io.File
+import java.io.IOException
 import java.nio.file.Files
+import java.nio.file.InvalidPathException
 import java.nio.file.Path
 import java.nio.file.Paths
 
-class Fuata(repoDir: Path = Paths.get(REPO_DIR).toAbsolutePath().normalize()) {
+class Fuata(private val repoDir: Path = Paths.get(REPO_DIR).toAbsolutePath().normalize()) {
 
     /**
      * Initialises a repository proper `.fuata` that will store all version history information
@@ -11,8 +19,8 @@ class Fuata(repoDir: Path = Paths.get(REPO_DIR).toAbsolutePath().normalize()) {
      */
     init {
         try {
-//            val currentDir = Paths.get(REPO_DIR).toAbsolutePath().normalize()
-            val fuataDir = repoDir.resolve(".fuata")
+            val fuataDir = repoDir.resolve(REPO_PROPER)
+            println("fuataDir: $fuataDir")
             // Create repository proper
             if (!Files.exists(fuataDir)) {
                 Files.createDirectory(fuataDir)
@@ -24,14 +32,17 @@ class Fuata(repoDir: Path = Paths.get(REPO_DIR).toAbsolutePath().normalize()) {
                 )
             }
 
-            val objectsDir = fuataDir.resolve("objects")
-            Files.createDirectory(objectsDir)
-            val refsDir = fuataDir.resolve("refs")
-            Files.createDirectory(refsDir)
-            val headPath = fuataDir.resolve("HEAD")
-            Files.createFile(headPath)
-            val indexPath = fuataDir.resolve("index")
-            Files.createFile(indexPath)
+            Files.createDirectory(fuataDir.resolve(OBJECTS_DIR))
+            Files.createDirectory(fuataDir.resolve(REFS_DIR))
+            Files.createDirectory(fuataDir.resolve("$REFS_DIR/heads"))
+            Files.createFile(fuataDir.resolve("$REFS_DIR/heads/main"))
+            Files.createFile(fuataDir.resolve(HEAD_FILE))
+            Files.createFile(fuataDir.resolve(INDEX_FILE))
+            // Write an initial main branch in the HEAD file
+            Files.writeString(fuataDir.resolve(HEAD_FILE), "refs/heads/main")
+            // Write an empty array to index file
+            val emptyJsonList = Json.encodeToString(emptyList<String>())
+            Files.writeString(fuataDir.resolve(INDEX_FILE), emptyJsonList)
             println("Initialised a Fuata directory in ${Paths.get(fuataDir.toString()).toAbsolutePath()}")
         } catch (e: FileAlreadyExistsException) {
             println(e.reason)
@@ -42,10 +53,37 @@ class Fuata(repoDir: Path = Paths.get(REPO_DIR).toAbsolutePath().normalize()) {
     /**
      * Invoked on the command `fuata add <filename>`
      * Adds a file to the staging area
+     * @param path File path of the file being staged
      */
-    fun add() {}
+    fun add(path: String) {
+        try {
+            val filePath = Paths.get(REPO_DIR, path)
+            val fuataDir = repoDir.resolve(REPO_PROPER)
+            val result = Indexing.addFileToIndex(
+                filePath = filePath.toString(),
+                objectDirectory = fuataDir.resolve(OBJECTS_DIR).toString(),
+                indexFile = fuataDir.resolve(INDEX_FILE).toString()
+            )
+            result.onSuccess {
+                println("success: added $path to staging index")
+            }.onFailure {
+                throw it
+            }
+        } catch (e: InvalidPathException) {
+            println("fatal: pathspec `$path` did not match any files")
+        } catch (e: IOException) {
+            println("fatal: pathspec `$path` did not match any files")
+        } catch (e: Exception) {
+            println("fatal: failed to stage file : ${e.message ?: ""}")
+        }
+    }
 
     private companion object {
         const val REPO_DIR = "."  // Current working directory
+        const val REPO_PROPER = ".fuata"
+        const val OBJECTS_DIR = "objects"
+        const val INDEX_FILE = "index"
+        const val REFS_DIR = "refs"
+        const val HEAD_FILE = "HEAD"
     }
 }
