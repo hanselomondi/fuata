466dead7c8a680816d8fa330b60437f39a507bc2
Hansel Omondi <ogopabhs@gmail.com>
2024-12-06 17:37:01 +0300

    Add: tasks.jar manifest in build.gradle
    This addition makes it possible to locate the entry point for the
    package. Additionally, the ShadowJar dependency has been added to enable
    the fat JAR when building the project. A fat JAR enables the inclusion
    of dependencies and libraries in the final build output so that no
    additional setup is needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.serialization") version "2.0.20"
     application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.example"
@@ -24,10 +25,22 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "21"
+kotlin {
+    jvmToolchain(21)
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to application.mainClass.get()
+        )
+    }
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveClassifier.set("") // Ensures the fat JAR has the default name
 }
