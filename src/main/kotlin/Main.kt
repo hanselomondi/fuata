5fe310dfa8f213c01d122c615c1151ae4b8af1c9
Hansel Omondi <ogopabhs@gmail.com>
2024-12-06 15:40:08 +0300

    Add: main function to handle terminal arguments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    try {
+        if (args.isEmpty()) {
+            println("fuata: missing command. Available commands: init, add, commit, log.")
+            return
+        }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+        val fuata = Fuata()
+
+        when (args[0]) {
+            "init" -> fuata.initialiseRepository()
+            "add" -> {
+                if (args.size < 2) {
+                    println("fuata: missing file name for 'add' command")
+                    println("Usage: fuata add <file_name>")
+                } else {
+                    fuata.add(args[1])
+                }
+            }
+            "commit" -> {
+                if (args.size < 3) {
+                    println("fuata: missing commit message")
+                    println("Usage: fuata commit \"<commit_message>\"")
+                } else {
+                    fuata.commit(args[2])
+                }
+            }
+            "log" -> fuata.log()
+            else -> println("fuata: unknown command ${args[1]}. \nAvailable commands: init, log, add, commit")
+        }
+    } catch (e: Exception) {
+        println("fuata: ${e.message ?: "unknown error"}")
+    }
 }
